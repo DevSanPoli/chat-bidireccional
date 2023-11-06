85fee1482d5f6d1578aa1e9bb942bb395f8110db
Aponte145 <55473254+Aponte145@users.noreply.github.com>
2023-11-06 15:36:25 -0500

    actualizacion

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import socket
 import threading
+from datetime import datetime
 
 
 # Servidor
 
+host = input("Ingresa la ip del servidor: ")
+port = int(input("Ingresa el puerto: "))
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 8080))
+server.bind((host, port))
 server.listen(5)
 
-print("El servidor está escuchando puerto 8080")
+print("se ha iniciado el servidor exitosamente Ip " + host + "puerto: " + str(port) )
 
 clientes = []
 
@@ -22,21 +26,33 @@ def broadcast(message, client_socket):
                 clientes.remove(client)
 
 
-def handle_client(client_socket):
-    while True:
-        try:
+def handle_client(client_socket, client_address):
+    try:
+        # Recibir el nombre de usuario del cliente
+        nombre_usuario = client_socket.recv(1024).decode('utf-8')
+        print(f"Cliente {nombre_usuario} ha iniciado sesión desde {client_address}")
+        
+        # Registrar el inicio de sesión en un archivo de registro
+        with open("registro_usuarios.txt", "a") as log_file:
+            log_file.write(f"{nombre_usuario} ha iniciado sesión desde {client_address} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+        while True:
             message = client_socket.recv(1024)
             if message:
                 broadcast(message, client_socket)
-        except:
-            clientes.remove(client_socket)
-            client_socket.close()
-            break
-
+    except Exception as e:
+        print(f"Cliente {nombre_usuario} ha abandonado el chat")
+        # Registrar el cierre de sesión en un archivo de registro
+        with open("registro_usuarios.txt", "a") as log_file:
+            log_file.write(f"{nombre_usuario} ha abandonado el chat - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+    finally:
+        clientes.remove(client_socket)
+        client_socket.close()
+        
 while True:
     client_socket, client_address = server.accept()
     clientes.append(client_socket)
-    print(f"Se ha establecido una conexión con {client_address}")
-    
-    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+
+    # Crear un hilo para manejar la comunicación con el cliente
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
     client_thread.start()
